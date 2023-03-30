20ba15f200cce91132bd2fca842ecf219a923a90
rickyshu <hoinleekk@naver.com>
2023-03-30 15:26:49 +0900

    design: Nav 컴포넌트에 페이지 아이콘 추가

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,19 +1,39 @@
-import styled from "styled-components";
-
+import styled, { css } from "styled-components";
+import { SlCalender } from "react-icons/sl";
+import { SlNotebook } from "react-icons/sl";
+import { BsChatSquareQuote } from "react-icons/bs";
+//밑에 누르면 나오도록 설정하면 될듯
 const Nav = () => {
   return (
     <NavBar>
-      {
-        //map을 사용해서 아이콘들을 정령하는 방식을 사용!
-      }
+      <CalenderIcon />
+      <Diary />
+      <WiseSaying />
     </NavBar>
   );
 };
-
+const CommonStyleIcons = css`
+  margin: 3rem;
+  font-size: 5rem;
+`;
 const NavBar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 50rem;
   height: 15rem;
   border-radius: 4rem;
   border: 1px solid red;
+`;
+const CalenderIcon = styled(SlCalender)`
+  ${CommonStyleIcons};
+`;
+
+const Diary = styled(SlNotebook)`
+  ${CommonStyleIcons};
+`;
+
+const WiseSaying = styled(BsChatSquareQuote)`
+  ${CommonStyleIcons};
 `;
 export default Nav;
