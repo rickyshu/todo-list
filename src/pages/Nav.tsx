3641f2d84fd09f1357618b6ae97fd44c462fd2c9
rickyshu <hoinleekk@naver.com>
2023-03-30 14:11:33 +0900

    feat:Nav 컴포넌트 생성

---
+++ src/pages/Nav.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <NavBar>
+      {
+        //map을 사용해서 아이콘들을 정령하는 방식을 사용!
+      }
+    </NavBar>
+  );
+};
+
+const NavBar = styled.nav`
+  width: 50rem;
+  height: 15rem;
+  border-radius: 4rem;
+  border: 1px solid red;
+`;
+export default Nav;
