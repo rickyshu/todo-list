c9e5342ddfc7054e6ae33efaae9bb383875b68b2
rickyshu <hoinleekk@naver.com>
2023-04-05 00:09:03 +0900

    feat:CalenderCell component 추가

--- src/routes/Calender.tsx
+++ src/routes/Calender.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 import CalenderHeader from "../components/CalenderHeader";
 import CalenderDay from "../components/CalenderDays";
+import CalenderCells from "../components/CalenderCells";
 import { format, addMonths, subMonths } from "date-fns";
 import { useState } from "react";
 const Calender = () => {
   const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
-
+  console.log(typeof format(new Date(), "d"));
   const preMonth = () => {
     setCurrentMonth(subMonths(currentMonth, 1));
   };
   const nextMonth = () => {
     setCurrentMonth(addMonths(currentMonth, 1));
   };
+  //타입 명시를 해줘야 한다.
+  const onDateClick = (day: Date) => {
+    setSelectedDate(day);
+  };
+
   return (
     <CalenderInfo>
       <CalenderHeader
@@ -21,15 +27,21 @@ const Calender = () => {
         nextMonth={nextMonth}
       ></CalenderHeader>
       <CalenderDay />
-      <div className="body">Cells</div>
+      <CalenderCells
+        currentMonth={currentMonth}
+        selectedDate={selectedDate}
+        onDateClick={onDateClick}
+      />
     </CalenderInfo>
   );
 };
 //calender 전체 크기를 지정한다.
 const CalenderInfo = styled.section`
-  width: 50rem;
-  height: 10rem;
-  outline: 2px dotted black;
+  width: 66rem;
+  height: 55rem;
+  //변경해야 할 부분이다.
+  outline: 0.25rem solid ${(prop) => prop.theme.style.CalenderOutline};
+  border-radius: 2rem;
 `;
 
 export default Calender;
