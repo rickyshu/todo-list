5d37273ced679334e61cb5e69f5a8c8be3e7a614
rickyshu <hoinleekk@naver.com>
2023-04-06 16:37:49 +0900

    fix: firebase추가 기능 삭제

--- src/components/Comfirm.tsx
+++ src/components/Comfirm.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ModalTodo from "./ModalTodo";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, TodoActions } from "../store";
 import { useEffect, useState } from "react";
@@ -8,26 +9,21 @@ import { connect } from "http2";
 //여기에서 ensure 버튼을 클릭하게 되면, 정보들이 다 firebase에 추가된다.
 //그러면 새로운 firebase의 정보를 기반으로
 const Comfirm = () => {
-  const [comfirm, setComfirm] = useState<boolean>(false);
-  const todoLists = useSelector((state: RootState) => state);
-  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
   //useEffect을 사용해서 비동기처리를 하고 싶다!
 
-  useEffect(() => {
-    //firebase에 정보를 저장하기!
-    console.log(todoLists);
-    addDoc(collection(db, "todolist-01"), {
-      ...todoLists,
-    });
-
-    dispatch(TodoActions.resetTodo());
-  }, [comfirm, todoLists, dispatch]);
-
-  const clickEventHandler = () => {
-    setComfirm((preval) => !preval);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
   };
 
-  return <ComfirmBotton onClick={clickEventHandler}>End The Day</ComfirmBotton>;
+  return (
+    <>
+      <ComfirmBotton onClick={toggleModal}>End The Day</ComfirmBotton>
+      <ModalTodo isOpen={isOpen} onClose={toggleModal}>
+        <p>확정하게 되면 수정이 불가능 합니다! 그대로 하시겠습니까?</p>
+      </ModalTodo>
+    </>
+  );
 };
 
 const ComfirmBotton = styled.button`
