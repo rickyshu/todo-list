603bbefe287b48fa5756b8f46b739acad04782bf
rickyshu <hoinleekk@naver.com>
2023-04-08 20:42:18 +0900

    feat:env 파일 생성 및 환경 변수 설정

--- src/components/Calender/ModalCalender.tsx
+++ src/components/Calender/ModalCalender.tsx
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
-const ModalCalender = () => {};
+const ModalCalender = () => {
+  return (
+    <ModalWrapper>
+      <ModalContent></ModalContent>
+    </ModalWrapper>
+  );
+};
 
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  width: 40rem;
+  height: 20rem;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5rem;
+  font-size: 1.4rem;
+  font-weight: 800;
+`;
 export default ModalCalender;
--- src/fbase.tsx
+++ src/fbase.tsx
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAof3slnIfAZLfClVpfb-lJDWH7sTgZAAM",
-  authDomain: "todolist-01-76383.firebaseapp.com",
-  projectId: "todolist-01-76383",
-  storageBucket: "todolist-01-76383.appspot.com",
-  messagingSenderId: "422920788126",
-  appId: "1:422920788126:web:273a257da04c75aea6038f",
-  measurementId: "G-PXP50SXM47",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASURMENTID,
 };
 
 // Initialize Firebase
